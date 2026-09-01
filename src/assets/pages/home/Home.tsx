ebb7423ca3db51b0cdfe009dfe910adaeff36b80
Edson Silva <dinhovdp@gmail.com>
2026-09-01 11:21:53 -0300

    :feat: integrate Tailwind CSS and add Navbar, Footer, and Home components

---
+++ src/assets/pages/home/Home.tsx
@@ -0,0 +1,35 @@
+
+function Home() {
+  return (
+    <>
+      <div className="bg-indigo-900 flex justify-center">
+        {/* Seção com 2 colunas */}
+        <div className="container grid grid-cols-1 md:grid-cols-2 text-white">
+          {/* Conteúdo de texto */}
+          <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <h2 className="text-2xl md:text-5xl font-bold">Seja Bem Vinde!</h2>
+
+            <p className="text-xl">Expresse aqui seus pensamentos</p>
+
+            <div className="flex justify-around gap-4">
+              <ModalPostagem />
+            </div>
+          </div>
+
+          {/* Imagem da página home */}
+          <div className="flex justify-center">
+            <img
+              src="https://i.imgur.com/fyfri1v.png"
+              alt="Imagem da página Home"
+              className="w-2/3"
+            />
+          </div>
+        </div>
+      </div>
+
+      <ListaPostagens />
+    </>
+  )
+}
+
+export default Home
